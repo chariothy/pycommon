301d5ab91dd79decf8823ee7c97adf1a191772ae
henry <henrytian@vip.qq.com>
2021-01-07 11:39:07 +0800

    Use package folder.

--- test/test_chariothy_common.py
+++ test/test_chariothy_common.py
@@ -1,12 +1,43 @@
 import unittest, os, logging
-import chariothy_common as cc
-from config_sample import CONFIG
-from exception import AppToolError
+
+from chariothy_common import deep_merge, deep_merge_in, benchmark, is_win, is_linux, is_macos, is_darwin, random_sleep, dump_json, load_json
+from config_test import CONFIG
+from chariothy_common import AppTool, AppToolError
+
+
+dict1 = {
+    'a1': 1, 
+    'a2': {
+        'b1': 2,
+        'b2': 3
+    }
+}
+dict_ori = {
+    'a1': 1, 
+    'a2': {
+        'b1': 2,
+        'b2': 3
+    }
+}
+dict2 = {
+    'a2': {
+        'b2': 4,
+        'b3': 5
+    }
+}
+dict3 = {
+    'a1': 1, 
+    'a2': {
+        'b1': 2,
+        'b2': 4,
+        'b3': 5
+    }
+}
 
 
 class CoreTestCase(unittest.TestCase):
     def setUp(self):
-        self.app_tool = cc.AppTool('test', os.getcwd(), config_name='config_sample')
+        self.app_tool = AppTool('test', os.getcwd(), config_name='config_test')
 
     def test_app_tool(self):
         logger = self.app_tool.init_logger()
@@ -15,102 +46,46 @@ class CoreTestCase(unittest.TestCase):
         self.assertLogs(logger, logging.ERROR)
 
     def test_deep_merge_in(self):
-        dict1 = {
-            'a1': 1, 
-            'a2': {
-                'b1': 2,
-                'b2': 3
-            }
-        }
-        dict2 = {
-            'a2': {
-                'b2': 4,
-                'b3': 5
-            }
-        }
-        dict3 = {
-            'a1': 1, 
-            'a2': {
-                'b1': 2,
-                'b2': 4,
-                'b3': 5
-            }
-        }
-        self.assertDictEqual(cc.deep_merge_in(dict1, dict2), dict3)
+        self.assertDictEqual(deep_merge_in(dict1, dict2), dict3)
         self.assertDictEqual(dict1, dict3)
 
     def test_deep_merge(self):
-        dict1 = {
-            'a1': 1, 
-            'a2': {
-                'b1': 2,
-                'b2': 3
-            }
-        }
-        dict_ori = {
-            'a1': 1, 
-            'a2': {
-                'b1': 2,
-                'b2': 3
-            }
-        }
-        dict2 = {
-            'a2': {
-                'b2': 4,
-                'b3': 5
-            }
-        }
-        dict3 = {
-            'a1': 1, 
-            'a2': {
-                'b1': 2,
-                'b2': 4,
-                'b3': 5
-            }
-        }
-        self.assertDictEqual(cc.deep_merge(dict1, dict2), dict3)
+        self.assertDictEqual(deep_merge(dict1, dict2), dict3)
         self.assertDictEqual(dict1, dict_ori)
 
     def test_is_win(self):
-        self.assertTrue(cc.is_win())
+        self.assertTrue(is_win())
 
     def test_is_linux(self):
-        self.assertFalse(cc.is_linux())
-
-    def test_get_home_dir(self):
-        self.assertEqual(cc.get_home_dir(), r'C:\Users\hytian3019')
+        self.assertFalse(is_linux())
     
     def test_is_macos(self):
-        self.assertFalse(cc.is_macos())
+        self.assertFalse(is_macos())
 
     def test_is_darwin(self):
-        self.assertFalse(cc.is_darwin())
+        self.assertFalse(is_darwin())
 
-    def test_get_win_folder(self):
-        self.assertEqual(cc.get_win_dir('Desktop'), r'C:\Users\hytian3019\Desktop')
-
-    @cc.benchmark
+    @benchmark
     def test_benchmark(self):
         import time
         time.sleep(1)
 
     def test_random_sleep(self):
-        cc.random_sleep()
+        random_sleep()
 
     def test_load_json(self):
-        self.assertIsNone(cc.load_json('/not-exist-file-path'))
+        self.assertIsNone(load_json('/not-exist-file-path'))
 
     def test_dump_json(self):
         file_path = './data/dump.json'
         data = {'test': 'OK'}
-        cc.dump_json(file_path, data)
+        dump_json(file_path, data)
         self.assertTrue(os.path.exists(file_path))
         
-        load_data = cc.load_json(file_path)
+        load_data = load_json(file_path)
         self.assertDictEqual(data, load_data)
 
         os.remove(file_path)
-    
     
     def test_get_config(self):
         """
